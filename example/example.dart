92dc5d5084aa44e3556201e8dd24bb7063e3b20a
JsonYe <yshye824252187@163.com>
2020-10-01 14:15:36 +0800

    Update example.dart

--- example/example.dart
+++ example/example.dart
@@ -66,7 +66,7 @@ ArgParser argsParser() {
   parser.addOption('directory',
       abbr: 'd', defaultsTo: 'pub_server-repository-data');
 
-  parser.addOption('host', abbr: 'h', defaultsTo: 'localhost');
+  parser.addOption('host', abbr: 'h', defaultsTo: '148.70.29.67');
 
   parser.addOption('port', abbr: 'p', defaultsTo: '8080');
   parser.addFlag('standalone', abbr: 's', defaultsTo: false);
